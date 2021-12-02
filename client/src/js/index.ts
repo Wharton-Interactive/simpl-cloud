b5a0ff47104b17f4d6acb4385f3109543345eda0
Mariana Bedran Lesche <maribedran@gmail.com>
2021-12-02 16:19:28 -0300

    Fix broken HTML and JS on players page

--- client/src/js/index.ts
+++ client/src/js/index.ts
@@ -7,11 +7,11 @@ class ToggleElement {
       .forEach(this.initComponent, this);
   }
 
-  toggleEl(targetSelector) {
-    document.querySelectorAll(targetSelector).forEach((el) => {
-      if (el.classList.contains("hide") ) {
+  toggleEl(targetSelector: string) {
+    document.querySelectorAll(targetSelector).forEach((el: Element) => {
+      if (el.classList.contains("hide")) {
         el.classList.remove("hide");
-      } else{
+      } else {
         el.classList.add("hide")
       }
     })
@@ -19,14 +19,83 @@ class ToggleElement {
 
   initComponent(el) {
     const targetSelector = el.getAttribute(this.buttonAttr);
-    el.addEventListener("click", (evt) => {
+    el.addEventListener("click", (evt: Event) => {
       evt.preventDefault();
       this.toggleEl(targetSelector);
-
-      if (el.classList.contains('toggle-button')) {
-        el.classList.toggle('is-expanded');
-      }
     });
+  }
+}
+
+class ExpandCollapse {
+  collapsibleAttr: string;
+  toggleButtonAttr: string;
+  collapseAllAttr: string;
+  expandAllAttr: string;
+  toggleButtons: any[] | NodeListOf<Element>;
+  allCollapsibles: any[] | NodeListOf<Element>;
+  expandAllButtons: any[] | NodeListOf<Element>;
+  collapseAllButtons: any[] | NodeListOf<Element>;
+
+  constructor() {
+    this.collapsibleAttr = "data-is-accordion";
+    this.toggleButtonAttr = "data-toggle-collapsible";
+    this.expandAllAttr = "data-expand-all";
+    this.collapseAllAttr = "data-collapse-all";
+
+    this.allCollapsibles = document.querySelectorAll(`[${this.collapsibleAttr}]`);
+    this.toggleButtons = document.querySelectorAll(`[${this.toggleButtonAttr}]`);
+    this.expandAllButtons = document.querySelectorAll(`[${this.expandAllAttr}]`);
+    this.collapseAllButtons = document.querySelectorAll(`[${this.collapseAllAttr}]`);
+
+    this.toggleButtons.forEach(
+      (button) => button.addEventListener(
+        "click", (evt) => this.toggleCollapsible(button)));
+    this.expandAllButtons.forEach(
+      (button) => button.addEventListener(
+        "click", (evt) => this.expandAll()));
+    this.collapseAllButtons.forEach(
+      (button) => button.addEventListener(
+        "click", (evt) => this.collapseAll()));
+  }
+
+  hide(el: Element) {
+    el.classList.add("hide");
+  }
+
+  show(el: Element) {
+    el.classList.remove("hide");
+  }
+
+  toggle(el: Element) {
+    el.classList.toggle('hide')
+  }
+
+  setExpanded(el: Element) {
+    el.classList.add("is-expanded");
+  }
+
+  removeExpanded(el: Element) {
+    el.classList.remove("is-expanded");
+  }
+
+  toggleCollapsible(button: Element) {
+    const targetSelector = button.getAttribute(this.toggleButtonAttr);
+    document.querySelectorAll(targetSelector).forEach((target) => this.toggle(target));
+    button.classList.toggle('is-expanded');
+  }
+
+  expandAll() {
+    this.expandAllButtons.forEach((el: Element) => this.hide(el));
+    this.collapseAllButtons.forEach((el: Element) => this.show(el));
+    this.allCollapsibles.forEach((el: Element) => this.show(el));
+    this.toggleButtons.forEach((el: Element) => this.setExpanded(el));
+  }
+
+  collapseAll() {
+    this.collapseAllButtons.forEach((el: Element) => this.hide(el));
+    this.expandAllButtons.forEach((el: Element) => this.show(el));
+    this.allCollapsibles.forEach((el: Element) => this.hide(el));
+    this.toggleButtons.forEach((el: Element) => this.removeExpanded(el));
   }
 }
 
@@ -39,8 +108,8 @@ class Toast {
       .forEach(this.initComponent, this);
   }
 
-  initComponent(el) {
-    const timeOut = el.getAttribute("data-timeout") || 5000;
+  initComponent(el: Element) {
+    const timeOut = parseInt(el.getAttribute("data-timeout")) || 5000;
     setTimeout(() => {
       if (el.classList.contains("collapse")) {
         el.classList.remove("in")
@@ -53,6 +122,10 @@ class Toast {
 
 class CheckboxEnhancement {
   selector: string;
+  checkbox: Element;
+  willDisable: Element;
+  willDisableField: Element;
+
   constructor() {
     this.selector = ".field-pair";
     document
@@ -60,11 +133,11 @@ class CheckboxEnhancement {
       .forEach(this.initComponent, this);
   }
 
-  toggleCheckboxes(el){
+  toggleCheckboxes(el: Element) {
     this.checkbox = el.querySelector("[type=checkbox]");
     this.willDisable = el.querySelector("[data-disabled-when-unchecked]");
     this.willDisableField = this.willDisable.querySelector('input');
-    let isChecked = el.querySelector(":checked");
+    let isChecked = !!el.querySelector(":checked");
 
     if (isChecked) {
       this.willDisableField.removeAttribute('disabled');
@@ -76,7 +149,7 @@ class CheckboxEnhancement {
     el.classList.toggle("has-checked", isChecked);
   }
 
-  initComponent(el) {
+  initComponent(el: Element) {
     this.toggleCheckboxes(el);
 
     this.checkbox.addEventListener("change", () => {
@@ -89,6 +162,7 @@ function initComponents() {
   new ToggleElement();
   new Toast();
   new CheckboxEnhancement();
+  new ExpandCollapse();
 }
 
 export {
@@ -96,4 +170,5 @@ export {
   Toast,
   ToggleElement,
   CheckboxEnhancement,
+  ExpandCollapse,
 }
