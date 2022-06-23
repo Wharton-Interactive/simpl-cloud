bed4f755424ace827feff1ba1a3b13de7c8d9fbd
Chris Beaven <smileychris@gmail.com>
2022-06-23 17:57:53 +1200

    Implement logic for min/max

--- client/src/components/AutoBalance.svelte
+++ client/src/components/AutoBalance.svelte
@@ -1,16 +1,31 @@
 <script>
   import { createEventDispatcher } from "svelte";
-
+  import { data } from "../balancing/stores";
   import Modal from "../components/Modal.svelte";
 
-  let fillCount = 3;
+  let fillCount;
+
+  function setFillCount(min, max) {
+    if (min && max) {
+      fillCount = Math.floor((max + min) / 2);
+    } else if (min) {
+      fillCount = Math.max(min, 3);
+    } else if (max) {
+      fillCount = Math.min(max, 3);
+    } else {
+      fillCount = 3;
+    }
+  }
+
+  // Set fillCount half way between minimum and maximum players if both are provided (or fall back to 3 if neither is provided).
+  $: setFillCount($data.minimumPlayers, $data.maximumPlayers);
 
   export let open;
   export let unassigned;
   const dispatch = createEventDispatcher();
 </script>
 
-<Modal bind:open={open}>
+<Modal bind:open>
   <div class="text-center">
     <form
       on:submit|preventDefault={() => {
@@ -19,20 +34,30 @@
       }}
     >
       <h2 class="modal-heading">Auto Assign</h2>
-
-      <!-- TODO: needs data and conditionals -->
-      <p>Fill or create teams between <strong>3</strong> and <strong>6</strong> players.</p>
-      
-      <!-- else... -->
-
       <p>
-        Fill or create teams of up to <input
-          class="auto-assign-input"
-          type="number"
-          min="1"
-          bind:value={fillCount}
-        />
-        players
+        Fill or create teams of up to {#if $data.minimumPlayers && $data.maximumPlayers && $data.minimumPlayers === $data.maximumPlayers}
+          {fillCount}{:else}
+          <input
+            class="auto-assign-input"
+            type="number"
+            min={$data.minimumPlayers || 1}
+            max={$data.maximumPlayers}
+            step="1"
+            bind:value={fillCount}
+          />{/if}
+        {fillCount == 1 ? "player" : "players"}
+        {#if $data.minimumPlayers !== $data.maximumPlayers}
+          {#if $data.minimumPlayers > 1 && $data.maximumPlayers}
+            <em
+              >(between {$data.minimumPlayers} and
+              {$data.maximumPlayers})</em
+            >
+          {:else if $data.minimumPlayers > 1}
+            <em>(at least {$data.minimumPlayers})</em>
+          {:else if $data.maximumPlayers}
+            <em>(at most {$data.maximumPlayers})</em>
+          {/if}
+        {/if}
       </p>
       <div class="button-wrap align-center">
         <button class="button">Assign {unassigned.length} Players</button>
