7ef718539aa5bc96be5db4c1b541936033006e03
Jhonatan Lopes <jhonatan.dapontelopes@gmail.com>
2023-05-05 10:01:44 -0300

    Add a test app

---
+++ simpl/testapp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from simpl import models as simpl_models
+
+
+class World(simpl_models.BaseInstance):
+    run = models.ForeignKey(
+        simpl_models.Run, blank=True, null=True, on_delete=models.SET_NULL
+    )
+
+
+class WorldWithRelatedName(simpl_models.BaseInstance):
+    run = models.ForeignKey(
+        simpl_models.Run,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="worldswithrelatedname",
+    )
